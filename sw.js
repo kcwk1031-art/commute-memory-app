049bfc603487ccbfbfba47837f690da5e7796919
KCW郭志維 <chihwei.kuo@car-plus.com.tw>
2026-09-24 23:31:24 +0800

    Bump public drive app to v35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "commute-drive-shell-v34";
+const SHELL_CACHE = "commute-drive-shell-v35";
 const SHELL_FILES = ["./", "./index.html", "./drive.css", "./drive.js", "./drive-destination.js", "./drive-direction.js", "./drive-guidance.js", "./drive-test-log.js", "./drive-starter-catalog.js", "./drive-config.js", "./official-cctv-catalog.json", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
